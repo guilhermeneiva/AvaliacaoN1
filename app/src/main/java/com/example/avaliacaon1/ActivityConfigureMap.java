67316e92a65192bf131c6b4d520e7617899af07b
Guilherme Neiva <guineiva51@gmail.com>
2025-04-17 19:26:44 -0300

    Botão visualizar mapa criado e adicionado startActivity

--- app/src/main/java/com/example/avaliacaon1/ActivityConfigureMap.java
+++ app/src/main/java/com/example/avaliacaon1/ActivityConfigureMap.java
@@ -1,6 +1,9 @@
 package com.example.avaliacaon1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,17 @@ public class ActivityConfigureMap extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_configure_map);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button viewMap = (Button) findViewById(R.id.button_viewMap);
+        viewMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ActivityConfigureMap.this, MapsActivity.class);
+                if (v.getId() == R.id.button_viewMap) {
+                    startActivity(i);
+                }
+            }
         });
+
     }
 }
